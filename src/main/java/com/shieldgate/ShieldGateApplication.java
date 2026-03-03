a718e59b7ee1dbdc23e26a54f5171e75a7e1df77
Yashashv <yashashvsinghxia1397@gmail.com>
2026-03-03 18:48:28 +0530

    Initial Commit

---
+++ src/main/java/com/shieldgate/ShieldGateApplication.java
@@ -0,0 +1,12 @@
+package com.shieldgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShieldGateApplication.class, args);
+    }
+}
